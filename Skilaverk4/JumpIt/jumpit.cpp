df6cc443c4c13f15c8a7c56fe5daf84771d0393e
baldurp12 <baldurp12@ru.is>
2016-02-17 16:59:16 +0000

    JumpIT kl 17

--- Skilaverk4/JumpIt/jumpit.cpp
+++ Skilaverk4/JumpIt/jumpit.cpp
@@ -3,3 +3,24 @@
 using namespace std;
 
 const int PENALTY = 1000;	// Used to assign a very high cost
+int cost = 0;
+
+int jumpIt(const int board[], int startIndex, int endIndex){
+    if(startIndex >= endIndex || startIndex +1 == endIndex || startIndex +2 == endIndex){
+        int sendInt = cost;
+        cost = 0;
+        return sendInt + board[endIndex];
+    }
+
+    else{
+        if(board[startIndex + 1] > board[startIndex + 2]){
+            cost += board[startIndex + 2];
+            return jumpIt(board, startIndex +2, endIndex);
+        }
+        else{
+            cost += board[startIndex + 1];
+            return jumpIt(board, startIndex +1, endIndex);
+        }
+    }
+
+};
