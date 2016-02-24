cb0aa7627af1bf70362bbfb94965a71c9edf5e63
baldurp12 <baldurp12@ru.is>
2016-02-24 14:49:34 +0000

    Ant & Bugs föll

--- Skilaverk5/PredatorPrey/Ant.h
+++ Skilaverk5/PredatorPrey/Ant.h
@@ -1,12 +1,20 @@
 #ifndef ANT_H
 #define ANT_H
+#include "Organism.h"
 
 
 class Ant : public Organism
 {
+
     public:
+        Ant (World* aWorld, int xCoord, int yCoord);
         void move();
         void breed();
+        virtual OrganismType getType();
+        virtual char representation();
+        virtual void generateOffspring(int whereX, int whereY);
+    private:
+        char charRep = 'A';
 
 };
 
--- Skilaverk5/PredatorPrey/Bug.h
+++ Skilaverk5/PredatorPrey/Bug.h
@@ -1,12 +1,23 @@
 #ifndef BUG_H
 #define BUG_H
+#include "Organism.h"
 
 
 class Bug : public Organism
 {
+
     public:
+        Bug (World* aWorld, int xCoord, int yCoord);
         void move();
         void breed();
+        virtual OrganismType getType();
+        virtual char representation();
+        virtual void generateOffspring(int whereX, int whereY);
+        int resetStarveCounter();
+        int getStarveCounter();
+    private:
+        int starveCounter;
+
 };
 
 #endif // BUG_H
