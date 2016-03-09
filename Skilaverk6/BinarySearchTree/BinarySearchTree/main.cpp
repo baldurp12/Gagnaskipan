17eaf3d0ff17b525fa9765254ca5642a77a90a59
Drokenz <meistarinn@gmail.com>
2016-03-09 13:37:55 +0000

    more files to ignore

---
+++ Skilaverk6/BinarySearchTree/BinarySearchTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinarySearchTree
+//
+//  Created by Valgeir Hrafn Snorrason on 09/03/16.
+//  Copyright © 2016 Droken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
