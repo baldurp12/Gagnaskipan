86cbf5a527c9133a1de83b1eeb58334e2a3df95c
Drokenz <meistarinn@gmail.com>
2016-03-16 17:19:29 +0000

    added functions to book.cpp

--- Skilaverk7/Books/book.cpp
+++ Skilaverk7/Books/book.cpp
@@ -1,1 +1,28 @@
 #include "book.h"
+
+Book::Book()
+{
+    
+}
+
+Book::Book(string anAuthor, string aTitle, string aDate)
+{
+    
+}
+string Book::getAuthor() const
+{
+    return 0;
+}
+
+string Book::getTitle() const
+{
+    return 0;
+}
+string Book::getDate() const
+{
+    return 0;
+}
+friend bool operator <(const Book &book1, const Book &book2)
+{
+    
+}
