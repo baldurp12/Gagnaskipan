c763d46bc466d4a6544b910421b9ff618755eb49
baldurp12 <baldurp12@ru.is>
2016-03-16 17:31:44 +0000

    Start of main

--- Skilaverk7/Books/main.cpp
+++ Skilaverk7/Books/main.cpp
@@ -17,3 +17,52 @@ void PrintBooks(vector<Book> &bookdata);
 void SortBooks(vector<Book> &bookdata);
 void PrintMenu();
 
+int main()
+{
+
+    int choice = 1;
+    vector<Book> bookVector;
+
+  while (choice != 3){
+        PrintMenu();
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1 :
+            AddNewBook(bookVector);
+            break;
+        case 2 :
+            PrintBooks(bookVector);
+            break;
+        case 3 :
+            break;
+        default:
+            break;
+        }
+  }
+
+}
+
+void PrintMenu(){
+    cout << "Select from the following choices:" << endl;
+    cout << "1. \t " << " Add new book" << endl;
+    cout << "2. \t " << " Print list sorted by author" << endl;
+    cout << "3. \t " << " Quit" << endl;
+}
+
+void AddNewBook(vector<Book> &bookdata)
+{
+
+}
+
+void PrintBooks(vector<Book> &bookdata)
+{
+    SortBooks(bookdata);
+
+}
+
+void SortBooks(vector<Book> &bookdata)
+{
+
+}
